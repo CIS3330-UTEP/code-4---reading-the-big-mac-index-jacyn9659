cedea9e2b09ba36536fad6f4d09ceca11795be95
jacyn9659 <143737150+jacyn9659@users.noreply.github.com>
2023-09-29 21:13:19 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -4,7 +4,7 @@ big_mac_file = './big-mac-full-index.csv'
 
 def get_big_mac_price_by_year(year,country_code):
     df = pd.read_csv(big_mac_file)
-    filtered_df = df[(df['date'] == year) & (df['iso_a3'] == coountry_code)]
+    filtered_df = df[(df['date'] == year) & (df['iso_a3'] == country_code)]
     mean_price = round(float(filtered_df['dollar_price'].mean()),2)
 
     return mean_price
@@ -38,10 +38,15 @@ def get_the_most_expensive_big_mac_price_by_year(year):
 if __name__ == "__main__":
     country = input("enter country code: ")
     year = int(input(" enter year: "))
-    get_big_mac_price_by_year(year, country)
-    get_big_mac_price_by_country(country)
-    get_the_cheapest_big_mac_price_by_year(year)
-    get_the_expensive_big_mac_by_price(year)
+    mean_price_year = get_big_mac_price_by_year(year, country)
+    mean_price_country = get_big_mac_price_by_country(country)
+    cheapest_price = get_the_cheapest_big_mac_price_by_year(year)
+    most_expensive_price = get_the_most_expensive_big_mac_price_by_year(year)
+    print(f"Mean Big Mac price in {country} in {year}: ${mean_price_year}")
+    print(f"Mean Big Mac price in {country}: ${mean_price_country}")
+    print(f"The cheapest Big Mac in {year}: {cheapest_price['name']}({cheapest_price['iso_a3']}): ${round(cheapest_price['dollar_price'], 2)}")
+    print(f"The most expensive Big Mac in {year}: {most_expensive_price['name']}({most_expensive_price['iso_a3']}): ${round(most_expensive_price['dollar_price'], 2)}")
+    
     
     
     
